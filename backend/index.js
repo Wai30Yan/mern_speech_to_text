5077c7ee628632d863d3cc52f86d332a58384968
Wai30Yan <42363348+Wai30Yan@users.noreply.github.com>
2022-02-14 23:19:19 +1100

    removing deprecated mongoose code snippet

--- backend/index.js
+++ backend/index.js
@@ -22,5 +22,3 @@ const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
-
-//mongoose.set('useFindAndModify', false)
