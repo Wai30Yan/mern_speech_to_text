957e37f82800b8723cafeab4a74ebf30d07e13f1
unknown <mr.waiyanmyint@gmail.com>
2022-02-22 10:46:44 +1100

    commit for jwt SECRET

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
 const auth = async (req, res, next) => {
     try {
@@ -9,7 +10,7 @@ const auth = async (req, res, next) => {
         let decodedData;
 
         if (token && isCustomAuth) {
-            decodedData = jwt.verify(token, 'Speech2Text')
+            decodedData = jwt.verify(token, process.env.SECRET)
             //console.log("decodedData", decodedData);
 
             req.userId = decodedData?.id
