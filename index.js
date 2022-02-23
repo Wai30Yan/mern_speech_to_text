554f18ab0d6f2e8dc74a4986434c911e08f55a45
unknown <mr.waiyanmyint@gmail.com>
2022-02-23 15:04:37 +1100

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ import speechRoute from './routes/speech.js'
 import userRoute from './routes/user.js'
 
 import path from 'path'
-import {fileURLToPath} from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-
-// const path = require("path")
 
 const app = express()
 dotenv.config()
@@ -23,16 +18,12 @@ app.use(cors())
 app.use('/speech', speechRoute)
 app.use('/user', userRoute)
 
-const __dirname = path.dirname(__filename);
-
-// app.use(express.static(__dirname));
 app.use(express.static(path.join('/frontend', '/build')));
 
 console.log(path.join('/frontend', '/build'))
 console.log("individual", express.static(path.join('/frontend', '/build')))
 
 app.get('*', (req, res) => {
-  // res.sendFile(path.join(__dirname, 'index.html'));
   res.sendFile(path.join('/frontend', '/build', 'index.html'));
 });
 
