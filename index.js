720dbb16ab93a515338ae0897f8edb483c7af8bd
unknown <mr.waiyanmyint@gmail.com>
2022-02-23 14:08:53 +1100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ import speechRoute from './routes/speech.js'
 import userRoute from './routes/user.js'
 
 import path from 'path'
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
 
 // const path = require("path")
 
@@ -20,11 +23,13 @@ app.use(cors())
 app.use('/speech', speechRoute)
 app.use('/user', userRoute)
 
-
-app.use(express.static(path.join(path.dirname('/frontend'), path.dirname('/build'))));
+const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname));
+// app.use(express.static(path.join(path.dirname('/frontend'), path.dirname('/build'))));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(path.dirname('/frontend'), path.dirname('/build'), 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
+  // res.sendFile(path.join(path.dirname('/frontend'), path.dirname('/build'), 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000
