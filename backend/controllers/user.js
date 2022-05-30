32741ba70c4b7ac54d42d94fe288421bfdd8ada1
unknown <mr.waiyanmyint@gmail.com>
2022-05-30 12:37:06 +1000

    remove unused package

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,7 +2,6 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
 import User from '../models/user.js'
-import dotenv from 'dotenv'
 
 export const signin = async (req, res) => {
     const { email, password } = req.body
